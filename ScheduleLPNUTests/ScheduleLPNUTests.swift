58bf290f164afe67ebe34e122f8c3f6277cd28ba
Denys <den.brativnyk@gmail.com>
2025-03-27 12:46:08 +0200

    Initial Commit

---
+++ ScheduleLPNUTests/ScheduleLPNUTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleLPNUTests.swift
+//  ScheduleLPNUTests
+//
+//  Created by Denys Brativnyk on 27.03.2025.
+//
+
+import Testing
+@testable import ScheduleLPNU
+
+struct ScheduleLPNUTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
